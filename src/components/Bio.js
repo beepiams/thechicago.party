d602d8a9e2397897773c534ce01ba59d2383fc96
beepiams <hi@beepia.ms>
2017-09-30 16:09:28 -0400

    styled components

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,22 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import profilePic from './gopartySignLogo_192.png'
+
+
 
 class Bio extends React.Component {
   render() {
     return (
       <p>
+        <Title>The Chicago Party</Title>
         <img
           src={profilePic}
           alt={`thechicago.party`}
         />
-        is the place to be...All Chicago Party Music{' '}
+        {/* is the place to be...All Chicago Party Music{' '}
         <a href="https://twitter.com/kylemathews">
         Follow / Like / Whatever
-        </a>
+        </a> */}
       </p>
     )
   }
 }
+
+const Title = styled.h1`
+	font-size: 1.5em;
+	text-align: center;
+	color: palevioletred;
+`;
 
 export default Bio
